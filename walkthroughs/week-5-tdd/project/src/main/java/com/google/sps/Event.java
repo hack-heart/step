cae181abc2570e5072a0c5b6c8fc9ba6bef7f589
Chibuzo <chibuzo.m.chiwike.22@dartmouth.edu>
2020-07-14 19:20:47 +0000

    Add a comparator for ordering events by start time

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/Event.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/Event.java
@@ -16,6 +16,7 @@ package com.google.sps;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -27,6 +28,16 @@ public final class Event {
   private final String title;
   private final TimeRange when;
   private final Set<String> attendees = new HashSet<>();
+
+  /**
+   * A comparator for sorting Events by their start time in ascending order.
+   */
+  public static final Comparator<Event> ORDER_BY_START_TIME = new Comparator<Event>() {
+    @Override
+    public int compare(Event a, Event b) {
+      return TimeRange.ORDER_BY_START.compare(a.when, b.when);
+    }
+  };
 
   /**
    * Creates a new event.
